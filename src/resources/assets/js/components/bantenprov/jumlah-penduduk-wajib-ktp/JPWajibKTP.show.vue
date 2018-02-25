27e5c8810716748b67cd51279927f997bc28c872
= <dikiwidia@live.com>
2018-02-25 09:00:38 +0700

    Update JSON Title, Echart Title, README.md

--- src/resources/assets/js/components/bantenprov/jumlah-penduduk-wajib-ktp/JPWajibKTP.show.vue
+++ src/resources/assets/js/components/bantenprov/jumlah-penduduk-wajib-ktp/JPWajibKTP.show.vue
@@ -2,7 +2,7 @@
   <div>
 
     <div class="card mb-3">
-      <div class="card-header">Grafik Produk Domestik Regional Bruto</div>
+      <div class="card-header">Grafik Jumlah Penduduk Wajib KTP</div>
       <div class="card-body">
         <echarts-jumlah-penduduk-wajib-ktp></echarts-jumlah-penduduk-wajib-ktp>
       </div><!-- /.card-body -->
@@ -11,7 +11,7 @@
     <div class="row">
       <div class="col-md-6">
         <div class="card mb-3">
-          <div class="card-header">Grafik Kota</div>
+          <div class="card-header">Grafik Jumlah Penduduk Wajib KTP Berdasarkan Kota</div>
           <div class="card-body">
             <echarts-jumlah-penduduk-wajib-ktp-kota></echarts-jumlah-penduduk-wajib-ktp-kota>
           </div><!-- /.card-body -->
@@ -19,7 +19,7 @@
       </div>
       <div class="col-md-6">
         <div class="card mb-3">
-          <div class="card-header">Grafik Tahunan</div>
+          <div class="card-header">Grafik Jumlah Penduduk Wajib KTP Berdasarkan Tahun</div>
           <div class="card-body">
             <echarts-jumlah-penduduk-wajib-ktp-tahun></echarts-jumlah-penduduk-wajib-ktp-tahun>
           </div><!-- /.card-body -->
--- src/resources/assets/js/components/views/bantenprov/jumlah-penduduk-wajib-ktp/DashboardJPWajibKTP.vue
+++ src/resources/assets/js/components/views/bantenprov/jumlah-penduduk-wajib-ktp/DashboardJPWajibKTP.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-
+    <div class="alert alert-info">
+      Jumlah Penduduk Wajib KT
+    </div>
     <div class="row">
       <div class="col-lg-6 mb-3">
         <div class="card h-100" style="color: #fff; background-color: #2196F3;">
@@ -25,7 +27,7 @@
         <div class="card h-100" style="color: #fff; background-color: #009688;">
           <div class="card-header">
             <i class="fa fa-bar-chart" aria-hidden="true"></i>
-            Anggaran dan Penyereapan Berdasarkan OPD
+            Jumlah Penduduk Wajib KTP
           </div>
           <div class="card-body">
             <jumlah-penduduk-wajib-ktp-bar-01></jumlah-penduduk-wajib-ktp-bar-01>
@@ -36,7 +38,7 @@
         <div class="card h-100" style="color: #fff; background-color: #FF9800;">
           <div class="card-header">
             <i class="fa fa-pie-chart" aria-hidden="true"></i>
-            Anggaran dan Penyereapan Berdasarkan OPD
+            Jumlah Penduduk Wajib KTP
           </div>
           <div class="card-body">
             <jumlah-penduduk-wajib-ktp-pie-01></jumlah-penduduk-wajib-ktp-pie-01>
@@ -52,7 +54,7 @@
         <div class="card h-100" style="color: #fff; background-color: #F44336;">
           <div class="card-header">
             <i class="fa fa-pie-chart" aria-hidden="true"></i>
-            Anggaran dan penyerapan per bulan tiap OPD
+            Jumlah Penduduk Wajib KTP
           </div>
           <div class="card-body">
             <jumlah-penduduk-wajib-ktp-pie-02></jumlah-penduduk-wajib-ktp-pie-02>
@@ -63,7 +65,7 @@
         <div class="card text-white bg-dark h-100">
           <div class="card-header">
             <i class="fa fa-bar-chart" aria-hidden="true"></i>
-            Anggaran dan penyerapan per bulan tiap OPD
+            Jumlah Penduduk Wajib KTP
           </div>
           <div class="card-body">
             <jumlah-penduduk-wajib-ktp-bar-02></jumlah-penduduk-wajib-ktp-bar-02>
